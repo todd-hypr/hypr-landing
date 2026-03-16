34d12ec9586e26953f8cdb678c62477f11fe64ff
Todd Nelson <todd@tntpr.com>
2026-03-16 13:33:56 -0700

    Update signup.js

--- functions/signup.js
+++ functions/signup.js
@@ -1,26 +1,3 @@
 export async function onRequest(context) {
-
-  if (context.request.method !== "POST") {
-    return new Response("Method Not Allowed", { status: 405 });
-  }
-
-  const body = await context.request.json();
-  const email = body.email;
-
-  await fetch("https://app.loops.so/api/v1/contacts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer 61cb1ef84df5e8e145e30521d1e7f706"
-    },
-    body: JSON.stringify({
-      email: email,
-      subscribed: true
-    })
-  });
-
-  return new Response(JSON.stringify({ success: true }), {
-    headers: { "Content-Type": "application/json" }
-  });
-
+  return new Response("Signup function is running");
 }
